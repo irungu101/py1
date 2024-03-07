0629d63a6be90a5fd493e678cc5597bfd1e8dc63
irungu101 <irungum36@gmail.com>
2024-03-07 14:56:10 +0300

    first commit

--- patients/views.py
+++ patients/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
-from .forms import StudentForm
-from .forms import EmployeeForm
+from django.shortcuts import render,redirect
+from patients.forms import EmployeeForm
+from patients.models import Student
+from patients.forms import StudentForm
+
+
 # Create your views here.
 def employee_form(request):
     form = EmployeeForm()
     return render(request, 'employee_form.html', {'form':form})
-def employee_list(request):
-    form = StudentForm()
-    if request.method == 'POST':
-        form = StudentForm(request.POST)
+
+def employee_list(request,id=0):
+    if request.method == 'GET':
+        if id == 0:
+            form = StudentForm()
+        else:
+            employee = Student.objects.get(pk=id)
+            form = StudentForm(instance=employee)
+        return render(request, 'employee_list.html', {'form':form})
+    else:
+        if id == 0:
+            form = StudentForm(request.POST)
+        else:
+            employee = Student.objects.get(pk=id)
+            form = StudentForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
-        else:
-            form = StudentForm()
-    return render(request, 'employee_list.html', {'form':form})
+        return redirect('/patient/show')
+
+
+def show(request):
+    context = {'show': Student.objects.all()}
+    return render(request, 'show.html', context)
+def delete(request,id):
+    employee = Student.objects.get(pk=id)
+    employee.delete()
+    return redirect('/student/show/')
